c307366682a8740980583e5bcbe33239124e06d2
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-08-09 13:46:17 +0800

    usign moment js library to check date input

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,7 +1,12 @@
 function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
+    var fname = document.forms["myForm"]["fname"].value;
+    var fdate = document.forms["myForm"]["fdate"].value;
+    if (fname == "") {
         alert("Name must be filled out");
         return false;
     }
+
+    if(!moment(fdate, "MM-DD-YYYY").isValid())
+        alert('Date must be MM-DD-YYYY format');
+
 }
