d61ceccef328fac95b1c39dd548d5e99206e9fa5
Billy <billychartanto@gmail.com>
2017-08-15 09:39:44 +0800

    coding convention

--- PBI2_OOP/test/src/test/main.java
+++ PBI2_OOP/test/src/test/main.java
@@ -1,12 +1,8 @@
 package test;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import java.util.*;
+
 import testPackage.test;
 
 public class main {
