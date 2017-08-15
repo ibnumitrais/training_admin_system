cebf1bc691181f2ebf6b43b1818822e6fc296b58
Billy <billychartanto@gmail.com>
2017-08-15 09:32:51 +0800

    inputoutputstream fix

--- inputoutput.java
+++ inputoutput.java
@@ -1,4 +1,4 @@
-package test;
+package asda1;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,16 +11,25 @@ public class main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		File file = new File("C:/test/text.txt");
+		File file = new File("C:/test/asda.txt");
 		file.createNewFile();
 		DataOutputStream stream = new DataOutputStream(new FileOutputStream(file));
-		stream.writeInt(123);
-		stream.close();
+		  byte[] b = {'a','b','c','d','e'};
+			stream.write(b);
 		
-		File file1 = new File("C:/test/text.txt");
-		DataInputStream stream1 = new DataInputStream(new FileInputStream(file));
-		int a = stream1.readInt();
-		System.out.println(a);
+			File file1 = new File("C:/test/text.txt");
+			DataInputStream stream1 = new DataInputStream(new FileInputStream(file));
+			
+			
+			int i;
+			 while((i = stream1.read())!=-1) {
+		         
+		            // converts integer to character
+		            char c = (char)i;
+		            
+		            // prints character
+		            System.out.print(c);
+		         }
 	}
 
 }
