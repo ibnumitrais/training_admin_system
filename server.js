5559c06fa9c9f7928ff27cc180082864d0eec6d8
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-08-28 10:45:27 +0800

    Initialize Express

--- server.js
+++ server.js
@@ -1,41 +1,18 @@
-var https = require("https");
-var http = require("http");
-var fs = require("fs");
-qs = require('querystring');
-var user = require("./model/user");
+var express = require("express");
 
-http.createServer(function(req, res) {
-	if (req.url === "/") {
-		res.writeHead(200, {"Content-Type": "text/plain"});
-		res.end("Homepage");
-	} else if (req.url === "/users") {
-		res.writeHead(200, {"Content-Type": "text/json"});
-		user.getRecords(res);
-	} else if (req.url === "/users/active") {
-		res.writeHead(200, {"Content-Type": "text/json"});
-		user.getActiveRecords(res);
-	} else if (req.url === "/users/inactive") {
-		res.writeHead(200, {"Content-Type": "text/json"});
-		user.getInactiveRecords(res);
-	} else if(req.url === "/users/add" && req.method === "GET") {
-		res.writeHead(200, {"Content-Type": "text/html"});
-	    fs.createReadStream("./public/form.html", "UTF-8").pipe(res);
-	} else if(req.url === "/users/add" && req.method === "POST") {
-		console.log("POST");
-		var body = '';
-		req.on('data', function (data) {
-			body += data;
-			console.log("Partial body: " + body);
-		});
-		req.on('end', function () {
-			qs = require('querystring');
-			
-			var json = qs.parse(body);
-			res.writeHead(200, {'Content-Type': 'text/html'});	
-			res.end(JSON.stringify(json));
-		});
-	} else {
-		res.writeHead(404, {"Content-Type": "text/plain"});
-		res.end("Whoops... Data not found");
-	}
-}).listen(3000);
+var app = express();
+
+
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}'`);
+	next();
+});
+
+app.use(express.static("./public"));
+
+
+app.listen(3000);
+
+console.log("Express app running on port 3000");
+
+module.exports = app;
