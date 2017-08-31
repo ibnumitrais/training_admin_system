b2a3845ffc8813540e987a82b633bb770875db5e
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-08-31 09:21:14 +0800

    combine the Nodejs server (Express and non express)

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
+//===========================> START EXPRESS
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
-var user = require("./model/user");
-var grade = require("./model/grade");
 var app = express();
-var path = require('path');
 
+// INITIALIZE EXPRESS APP
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function(req, res, next) {
-	// console.log(`${req.method} request for '${req.url}'`);
-	next();
-});
-
+app.use(function(req, res, next) {next();});
 app.use(express.static("./public"));
-
 app.use(cors());
 
-//GRADES
-app.get('/grades', function (req, res) {
-	res.sendFile(path.join(__dirname + '/public/grade.html'));
-})
+//MODEL
+var user = require("./model/user");
+var grade = require("./model/grade");
 
+//GRADES ROUTE
+app.get('/grades', function (req, res) {
+	fs.createReadStream("./views/grade.html", "UTF-8").pipe(res);
+})
 app.get("/grade-api", function(req, res) {
 	grade.getRecords(function(status, data){
 		if(status > 0) {
@@ -66,13 +62,10 @@ app.get("/grade-api", function(req, res) {
 				"data": recordsFiltered,
 			});
 		}
-		else {
-			console.log(data);
-		}
+		else {console.log(data);}
 	}, '');
 });
 app.post("/grade-api", function(req, res) {
-	var filter = req.body.filter;
     grade.storeRecord(function(status, message){
 		if(status > 0) res.json(1);
 		else res.json(-1);
@@ -85,14 +78,13 @@ app.delete("/grade-api/:id", function(req, res) {
 	}, req.param("id"));
 });
 
-//USER
+//USER ROUTE
 app.get('/users', function (req, res) {
-	res.sendFile(path.join(__dirname + '/public/user.html'));
+	fs.createReadStream("./views/user.html", "UTF-8").pipe(res);
 })
 app.get("/user-api", function(req, res) {
 	user.getRecords(function(status, data){
 		if(status > 0) {
-			
 			var draw 		= req.query.draw;
 			var start 		= req.query.start;
 			var length 		= req.query.length;
@@ -120,7 +112,7 @@ app.get("/user-api", function(req, res) {
 					var nameB = a[orderNameColumn].toUpperCase(); 
 					var nameA = b[orderNameColumn].toUpperCase(); 
 				}
-				
+
 				if (nameA < nameB) return -1;
 				if (nameA > nameB) return 1;
 				return 0;
@@ -134,13 +126,10 @@ app.get("/user-api", function(req, res) {
 				"data": recordsFiltered,
 			});
 		}
-		else {
-			console.log(data);
-		}
+		else {console.log(data);}
 	}, '');
 });
 app.post("/user-api", function(req, res) {
-	var filter = req.body.filter;
     user.storeRecord(function(status, message){
 		if(status > 0) res.json(1);
 		else res.json(-1);
@@ -152,10 +141,95 @@ app.delete("/user-api/:userid", function(req, res) {
 		else res.json(-1);
 	}, req.param("userid"));
 });
-
-
 app.listen(3000);
-
 console.log("Express app running on port 3000");
-
 module.exports = app;
+//===========================> END EXPRESS
+////////////////////////////
+////////////////////////////
+////////////////////////////
+//===========================> START NON EXPRESS
+var https = require("https");
+var http = require("http");
+var ejs = require('ejs');
+var fs = require("fs");
+var qs = require("querystring");
+
+var user = require("./model/user");
+http.createServer(function(req, res) {
+    if(req.method === "GET") {
+        if (req.url === "/") {
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(ejs.render(fs.readFileSync('./views/index.ejs', 'utf8'), { 
+				filename: 'index.ejs'
+			}));
+		} else {
+			// READ the assets
+            fs.readFile('./public' + req.url, function(err, data) {
+                if (!err) {
+                    var dotoffset = req.url.lastIndexOf('.');
+                    var mimetype = dotoffset == -1
+                                    ? 'text/plain'
+                                    : {
+                                        '.html' 	: 'text/html',
+                                        '.ico' 		: 'image/x-icon',
+                                        '.jpg' 		: 'image/jpeg',
+                                        '.png' 		: 'image/png',
+                                        '.gif' 		: 'image/gif',
+                                        '.css' 		: 'text/css',
+                                        '.js' 		: 'text/javascript',
+                                        '.woff2'	: 'font/opentype',
+                                        '.woff' 	: 'font/opentype',
+                                        '.ttf' 		: 'font/opentype',
+                                        }[ req.url.substr(dotoffset) ];
+                    
+                    res.setHeader("Content-Type", mimetype);
+                    res.write(data);
+                    res.end();
+                } else {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('Cannot ' + req.method + ' ' + req.url);
+                }
+            });
+        }
+    }
+    else if(req.method === "POST") {
+       if(req.url === '/requestURL') {
+			var body = "";
+			req.on("data", function(chunk) {
+				body += chunk;
+			});
+
+			req.on("end", function(data) {
+				var reqJson = qs.parse(body);
+				var options = {
+					hostname: reqJson.pbi2_url,
+					path: reqJson.pbi2_path,
+					method: "GET"
+				};
+
+				var urlReq = https.request(options, function(urlRes) {
+					var responseBody = "";
+					urlRes.setEncoding("UTF-8");	
+					urlRes.on("data", function(chunk) {
+						responseBody += chunk;
+					});
+					urlRes.on("end", function() {
+						res.setHeader('Content-Type', 'text/html');
+						res.end(responseBody);
+					});
+				});
+				
+				urlReq.on("error", function(err) {
+					console.log(err);
+					res.end("err");
+				});
+				
+				urlReq.end();
+			});
+        }
+    }
+
+}).listen(3001);
+console.log("Non Express app running on port 3001");
